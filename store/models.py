51aadefa7148a8ae410fbc3d2c6bf9e3fa135e74
vadherusha <~vadherusha0003@gmail.com>
2022-11-27 21:40:22 +0530

    Eshop

---
+++ store/models.py
@@ -0,0 +1,116 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Address(models.Model):
+    user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
+    locality = models.CharField(max_length=150, verbose_name="Nearest Location")
+    city = models.CharField(max_length=150, verbose_name="City")
+    state = models.CharField(max_length=150, verbose_name="State")
+
+    def __str__(self):
+        return self.locality
+
+class Checkout(models.Model):
+    ch_id=models.AutoField(primary_key=True)
+    firstname=models.CharField(max_length=12,default="" ,verbose_name="first name")
+    lastname=models.CharField(max_length=15,default="",verbose_name="last name")
+    email=models.CharField(max_length=45,default="",verbose_name="email")
+    phone = models.PositiveIntegerField(default="",verbose_name="phone number")
+    companyname=models.CharField(max_length=70, default="",verbose_name="company name")
+    country=models.CharField(max_length=45, default="",verbose_name="country name")
+    town=models.CharField(max_length=157, default="",verbose_name="town name")
+    state=models.CharField(max_length=15, default="",verbose_name="state name")
+     
+    def __str__(self):
+        return self.firstname
+    
+    def __str__(self):
+        return self.ch_id
+    
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Category Title")
+    slug = models.SlugField(max_length=55, verbose_name="Category Slug")
+    description = models.TextField(blank=True, verbose_name="Category Description")
+    category_image = models.ImageField(upload_to='category', blank=True, null=True, verbose_name="Category Image")
+    is_active = models.BooleanField(verbose_name="Is Active?")
+    is_featured = models.BooleanField(verbose_name="Is Featured?")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created Date")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated Date")
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        ordering = ('-created_at', )
+
+    def __str__(self):
+        return self.title
+
+class Contact(models.Model):
+    msg_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=70, default="")
+    phone = models.CharField(max_length=70, default="")
+    desc = models.CharField(max_length=500, default="")
+
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    title = models.CharField(max_length=150, verbose_name="Product Title")
+    slug = models.SlugField(max_length=160, verbose_name="Product Slug")
+    sku = models.CharField(max_length=255, unique=True, verbose_name="Unique Product ID (SKU)")
+    short_description = models.TextField(verbose_name="Short Description")
+    detail_description = models.TextField(blank=True, null=True, verbose_name="Detail Description")
+    product_image = models.ImageField(upload_to='product', blank=True, null=True, verbose_name="Product Image")
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, verbose_name="Product Categoy", on_delete=models.CASCADE)
+    is_active = models.BooleanField(verbose_name="Is Active?")
+    is_featured = models.BooleanField(verbose_name="Is Featured?")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created Date")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated Date")
+
+    class Meta:
+        verbose_name_plural = 'Products'
+        ordering = ('-created_at', )
+
+    def __str__(self):
+        return self.title
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, verbose_name="Product", on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1, verbose_name="Quantity")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created Date")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated Date")
+
+    def __str__(self):
+        return str(self.user)
+    
+    # Creating Model Property to calculate Quantity x Price
+    @property
+    def total_price(self):
+        return self.quantity * self.product.price
+
+
+STATUS_CHOICES = (
+    ('Pending', 'Pending'),
+    ('Accepted', 'Accepted'),
+    ('Packed', 'Packed'),
+    ('On The Way', 'On The Way'),
+    ('Delivered', 'Delivered'),
+    ('Cancelled', 'Cancelled')
+)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
+    address = models.ForeignKey(Address, verbose_name="Shipping Address", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, verbose_name="Product", on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(verbose_name="Quantity")
+    ordered_date = models.DateTimeField(auto_now_add=True, verbose_name="Ordered Date")
+    status = models.CharField(
+        choices=STATUS_CHOICES,
+        max_length=50,
+        default="Pending"
+        )
